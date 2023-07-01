32e70d54716eeb2946959dd0a6865dd9f2cee1a9
Kyaw Thu <evanch98@gmail.com>
2023-07-01 19:20:44 +0800

    add tests

--- app/src/test/java/com/example/assignment_part1/BmiCalculatorTest.kt
+++ app/src/test/java/com/example/assignment_part1/BmiCalculatorTest.kt
@@ -1,5 +1,34 @@
 package com.example.assignment_part1
 
-import org.junit.jupiter.api.Assertions.*
+import org.junit.Assert.assertEquals
+import org.junit.Test
 
-internal class BmiCalculatorTest
+class BmiCalculatorTest {
+  @Test
+  fun resultTest1() {
+    val bmiCalculator = BmiCalculator(136, 67.0, "male")
+    val result: BmiResult = bmiCalculator.getResult()
+    assertEquals(BmiResult.NORMAL, result)
+  }
+
+  @Test
+  fun resultTest2() {
+    var bmiCalculator = BmiCalculator(90, 67.0, "female")
+    val result: BmiResult = bmiCalculator.getResult()
+    assertEquals(BmiResult.UNDERWEIGHT, result)
+  }
+
+  @Test
+  fun resultTest3() {
+    var bmiCalculator = BmiCalculator(180, 67.0, "male")
+    val result: BmiResult = bmiCalculator.getResult()
+    assertEquals(BmiResult.OVERWEIGHT, result)
+  }
+
+  @Test
+  fun resultTest4() {
+    var bmiCalculator = BmiCalculator(200, 67.0, "male")
+    val result: BmiResult = bmiCalculator.getResult()
+    assertEquals(BmiResult.OBESE, result)
+  }
+}
