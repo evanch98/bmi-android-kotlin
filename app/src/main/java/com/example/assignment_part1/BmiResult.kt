f01cc8dd55567ddcd8a64434573dd1f4b754a013
Kyaw Thu <evanch98@gmail.com>
2023-07-01 18:49:52 +0800

    add enum constants

--- app/src/main/java/com/example/assignment_part1/BmiResult.kt
+++ app/src/main/java/com/example/assignment_part1/BmiResult.kt
@@ -1,4 +1,8 @@
 package com.example.assignment_part1
 
-enum class BmiResult {
+enum class BmiResult(val text: String) {
+  UNDERWEIGHT("You are underweight!"),
+  NORMAL("Your BMI is normal."),
+  OVERWEIGHT("You are overweight!"),
+  OBESE("You are obese!!!")
 }
