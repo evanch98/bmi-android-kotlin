62e59d23e7a3ebfd4f64701c61d10f0d41c1f992
Kyaw Thu <evanch98@gmail.com>
2023-07-01 18:16:33 +0800

    initialize the BmiCalculator class

--- app/src/main/java/com/example/assignment_part1/BmiCalculator.kt
+++ app/src/main/java/com/example/assignment_part1/BmiCalculator.kt
@@ -1,4 +1,12 @@
 package com.example.assignment_part1
 
-class BmiCalculator {
+class BmiCalculator(val weight: Int, val height: Int, gender: String) {
+  lateinit var result: String;
+  fun getResult(): String {
+    return result;
+  }
+
+  private fun calculate() {
+
+  }
 }
