8fafbbff8e1cebd42c00ccaaddde281f813c1f1d
Kyaw Thu <evanch98@gmail.com>
2023-07-01 19:49:48 +0800

    update testing

--- app/src/test/java/com/example/assignment_part1/BmiCalculatorTest.kt
+++ app/src/test/java/com/example/assignment_part1/BmiCalculatorTest.kt
@@ -6,28 +6,28 @@ import org.junit.Test
 class BmiCalculatorTest {
   @Test
   fun resultTest1() {
-    val bmiCalculator = BmiCalculator(136, 67.0, "male")
+    val bmiCalculator = BmiCalculator(136, 67.0)
     val result: BmiResult = bmiCalculator.getResult()
     assertEquals(BmiResult.NORMAL, result)
   }
 
   @Test
   fun resultTest2() {
-    var bmiCalculator = BmiCalculator(90, 67.0, "female")
+    var bmiCalculator = BmiCalculator(90, 67.0)
     val result: BmiResult = bmiCalculator.getResult()
     assertEquals(BmiResult.UNDERWEIGHT, result)
   }
 
   @Test
   fun resultTest3() {
-    var bmiCalculator = BmiCalculator(180, 67.0, "male")
+    var bmiCalculator = BmiCalculator(180, 67.0)
     val result: BmiResult = bmiCalculator.getResult()
     assertEquals(BmiResult.OVERWEIGHT, result)
   }
 
   @Test
   fun resultTest4() {
-    var bmiCalculator = BmiCalculator(200, 67.0, "male")
+    var bmiCalculator = BmiCalculator(200, 67.0)
     val result: BmiResult = bmiCalculator.getResult()
     assertEquals(BmiResult.OBESE, result)
   }
